bbd5bb728dd7cb74e31f49781b650a928a026ba8
carlo <ecaliwanag@gmail.com>
2020-07-12 22:46:14 +0800

    added pdf view and logout to dashboard

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PdfViewerService } from './../services/pdf-viewer.service';
+import { UsersService } from './../services/users.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  pdfURL = 'https://assets.biomarking.com/images/sample.pdf';
+  pdfName = 'sample.pdf';
 
-  constructor() { }
+  constructor(
+    private pdfViewerSvc: PdfViewerService,
+    private usersSvc: UsersService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  viewPDF() {
+    this.pdfViewerSvc.download(this.pdfURL, this.pdfName);
   }
 
+  logout() {
+    this.usersSvc.isLoggedIn = false;
+    this.router.navigate(['/login']);
+  }
 }
